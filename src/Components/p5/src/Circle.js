bc0367ee60eee312d98c7db282c9422310850fa9
Juanse2296 <juan-2296@hotmail.es>
2017-10-28 17:06:17 -0500

    background interaction

--- src/Components/p5/src/Circle.js
+++ src/Components/p5/src/Circle.js
@@ -1,70 +1,98 @@
 class Circle {
-  constructor(x, y, size, timeVisible, timeColor, r,g,b) {
-    this.x = x
-    this.y = y
-    this.size = size
-    this.tempoSize = size
-    this.velocity = this.getRandomArbitrary(0.1, 0.5)
-    this.opacity = this.getRandomArbitrary(50, 255)
-    this.visible=true
-    this.changeColor=true
-    this.timeVisible = timeVisible
-    this.timeColor = timeColor
-    this.r=r
-    this.g=g
-    this.b=b
-    this.dif =this.getRandomArbitrary(1, 30)
-  }
-  draw(app) {
-    app.fill(this.r,this.g, this.b,this.opacity)
-    let posX = this.x
-    let posY = this.y
-    posX=posX+app.map(app.mouseX, 0,app.width,0,this.dif)
-    app.ellipse(posX, posY, this.size, this.size)
-    if(this.move())this.y=app.height
-    this.changeEffect()
-    this.timeEffects(app)
-  }
-  move() {
-    this.y -= this.velocity
-    if (this.y < 0) return true
-  }
-
-  getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min
-  }
-
-  changeEffect(){
-    if(this.visible && this.opacity>-1){
-      this.opacity--
-      if(this.size>=0)this.size-=0.01
-    }else{
-      this.opacity++
-      if(this.size<=this.tempoSize)this.size+=0.01
+    constructor(app, x, y) {
+        this.x = x
+        this.y = y
+        this.size = this.getRandomArbitrary(0.75, 3)
+        this.tempoSize = this.size
+        this.velocity = this.getRandomArbitrary(0.1, 0.5)
+        this.opacity = this.getRandomArbitrary(50, 255)
+        this.visible = true
+        this.changeColor = true
+        this.timeVisible =  app.ceil(this.getRandomArbitrary(600, 300))
+        this.timeColor = app.ceil(this.getRandomArbitrary(600, 1200))
+        this.r = 255
+        this.g = 255
+        this.b = 255
+        this.dif = this.getRandomArbitrary(1, 30)
     }
 
-    if(this.changeColor){
-      if(this.r<255)this.r++
-      if(this.g<255)this.g++
-      if(this.b<255)this.b++
-    }else{
-      if(this.r>0)this.r--
-      if(this.g>239)this.g--
-      if(this.b>240)this.b--
+    draw(app) {
+        let posX = this.x
+        let posY = this.y
+        posX = posX + (app.map(app.mouseX, 0, app.width, 0, this.dif) * -1)
+        let opa=this.opacity
+        let s = this.size
+        let dis = app.dist(posX, posY, app.mouseX, app.mouseY)
+        this.draw_object(app,s,opa,posX, posY,dis)
+        if (this.move()) this.y = app.height
+        this.changeEffect()
+        this.timeEffects(app)
+        this.draw_line(app,posX,posY,opa,dis)
     }
-  }
 
-  timeEffects  (app){
-    if(app.frameCount%this.timeColor===0){
-      this.changeColor=!this.changeColor
+    draw_object(app,size, opacity, posX, posY, dist){
+        let newSize = size
+        if(dist<100){
+               newSize+=app.map(dist,100,0,0,size*1.5)
+        }
+        app.fill(this.r, this.g, this.b, opacity)
+        app.ellipseMode(app.CENTER)
+        app.ellipse(posX, posY, newSize, newSize)
     }
-    if(app.frameCount%this.timeVisible===0){
-      this.visible=!this.visible
+
+    draw_line(app, posX, posY, opacity,dist){
+        app.push()
+        if(dist<100 && opacity>50){
+            let opaLine = app.map(dist, 100,0,0,255)
+            app.stroke(this.r, this.g, this.b,opaLine)
+            app.strokeWeight(.5)
+            app.line(posX, posY, app.mouseX, app.mouseY)
+        }
+        app.pop()
     }
-  }
+
+    move() {
+        this.y -= this.velocity
+        if (this.y < 0) return true
+    }
+
+    getRandomArbitrary(min, max) {
+        return Math.random() * (max - min) + min
+    }
+
+    changeEffect() {
+        if (this.visible && this.opacity > -1) {
+            this.opacity--
+            if (this.size >= 0) this.size -= 0.01
+        } else {
+            this.opacity++
+            if (this.size <= this.tempoSize) this.size += 0.01
+        }
+        if (this.changeColor) {
+            if (this.r < 255) this.r++
+            if (this.g < 255) this.g++
+            if (this.b < 255) this.b++
+        } else {
+            if (this.r > 0) this.r--
+            if (this.g > 239) this.g--
+            if (this.b > 240) this.b--
+        }
+    }
+
+    timeEffects(app) {
 
 
+        if (app.frameCount % this.timeColor === 0) {
+            this.changeColor = !this.changeColor
+        }
+        if (app.frameCount % this.timeVisible === 0) {
+            this.visible = !this.visible
+        }
+
+
+    }
 
 
 }
+
 export default Circle
--- src/Components/p5/src/index.js
+++ src/Components/p5/src/index.js
@@ -1,67 +1,68 @@
 'use strict'
 import Hexa from './Hexa'
 import Circle from './Circle'
-import p5 from 'p5'
-import path from 'path'
+import background from '../../../assets/Images/background.png'
 
 export default function sketch(p) {
 
-  let hexas = []
-  let circles = []
-  let font=null;
+    let hexas = []
+    let circles = []
+    let font = null;
+    let img;
 
-  p.setup = () => {
-    font = 'Georgia'
-    p.createCanvas(window.innerWidth, window.innerHeight)
-    for (let i = 0; i < 500; i++) {
-      let y = getRandomArbitrary(1, p.height - 1)
-      createCircle(y)
+    p.setup = () => {
+        font = 'Georgia'
+        img = p.loadImage(background);
+        p.createCanvas(window.innerWidth, window.innerHeight)
+
     }
-  }
 
-  p.draw = () => {
-    p.background(32,37,48,95);
-    circlesCycle()
-  }
-  function circlesHexas() {
-    p.noStroke()
-    hexas.forEach(function (hexa, i) {
-      hexa.draw(p)
-    })
-  }
+    setInterval(
+         ()=> {
+           if(circles.length<500){
+               let y = getRandomArbitrary(1, p.height - 1)
+               createCircle(y)
+           }
+        }, 100);
 
-  function circlesCycle() {
-    p.noStroke()
-    circles.forEach(function (circle, i) {
-      circle.draw(p)
-    })
-  }
+    p.draw = () => {
+        p.background(32, 37, 48, 95);
+        //   p.background(img,95);
+        circlesCycle()
+    }
 
-  function createCircle(y) {
-    let x = getRandomArbitrary(1, p.width - 1)
-    let size = getRandomArbitrary(0.75, 3)
-    let timeVisible = p.ceil(getRandomArbitrary(600, 300))
-    let timeColor = p.ceil(getRandomArbitrary(600, 3600))
-    //  let col = p.color(0, 196, 191)
-    let r = 255
-    let g = 255
-    let b = 255
-    let circle = new Circle(x, y, size,timeVisible,timeColor,r,g,b)
-    circles.push(circle)
-  }
+    function circlesHexas() {
+        p.noStroke()
+        hexas.forEach(function (hexa, i) {
+            hexa.draw(p)
+        })
+    }
 
-  function createHexas(y) {
-    let x = getRandomArbitrary(1, p.width - 1)
-    let hexa = new Hexa(x, y, getRandomArbitrary(0.25, 3))
-    hexas.push(hexa)
-  }
+    function circlesCycle() {
+        p.noStroke()
+        circles.forEach(function (circle, i) {
+            circle.draw(p)
+        })
+    }
 
-  function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min
-  }
+    function createCircle(y) {
+        let x = getRandomArbitrary(1, p.width - 1)
+        let circle = new Circle(p, x, y)
+        circles.push(circle)
+    }
 
-  p.windowResized = () => {
-    p.resizeCanvas(window.innerWidth, window.innerHeight)
-  }
+    function createHexas(y) {
+        let x = getRandomArbitrary(1, p.width - 1)
+        let hexa = new Hexa(x, y, getRandomArbitrary(0.25, 3))
+        hexas.push(hexa)
+    }
+
+    function getRandomArbitrary(min, max) {
+        return Math.random() * (max - min) + min
+    }
+
+    p.windowResized = () => {
+        p.resizeCanvas(window.innerWidth, window.innerHeight)
+    }
 
 }

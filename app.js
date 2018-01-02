e5eae405f4e71c9f599738fab4d2d40d508513d1
Juan Sebastian Reina <juan-2296@hotmail.es>
2018-01-02 18:51:12 -0500

    production v3

--- app.js
+++ app.js
@@ -32,7 +32,9 @@ app.use(_bodyParser2.default.urlencoded({ extended: false }));
 app.use(_bodyParser2.default.json());
 var env = process.env.NODE_ENV || 'dev';
 var DIST_DIR = _path2.default.join(__dirname, '../dist');
-if (env === 'production') DIST_DIR = _path2.default.join(__dirname, './dist');
+if (env === 'production') {
+  DIST_DIR = _path2.default.join(__dirname, './dist');
+}
 app.use(_express2.default.static(DIST_DIR));
 app.use('/api', _routes2.default);
 app.get('*', function (req, res) {

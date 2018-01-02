b80253454dea697b8a4b0880ed40147ee2beca02
Juan Sebastian Reina <juan-2296@hotmail.es>
2018-01-01 23:15:54 -0500

    refractoring head tags

---
+++ src/Components/Common/Head_tags/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Links from './links';
+import Meta from './meta';
+
+const Head = () => (
+  <div>
+    <Links />
+    <Meta />
+  </div>
+);
+
+export default Head;
+

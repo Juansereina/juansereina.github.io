8a2e693846d2550c247ca12079572daf4c45f2a9
Julian Madrid <juan-2296@hotmail.es>
2018-04-07 22:46:27 -0500

    changing p5 to threejs

--- src/app.jsx
+++ src/app.jsx
@@ -2,22 +2,15 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import { AnimatedSwitch } from 'react-router-transition';
 import Favicon from 'react-favicon';
-import { UserAgentProvider, UserAgent } from '@quentin-sommer/react-useragent';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import Work from './Components/Work';
 import Contact from './Components/Contact';
-import Background from './Components/p5';
 import Meta from './Components/Common/Head_tags';
 
 const App = () => (
   <div>
     <Meta />
-    <UserAgentProvider ua={window.navigator.userAgent}>
-      <UserAgent mobile>
-        {uaIsMobile => (!uaIsMobile && <Background />)}
-      </UserAgent>
-    </UserAgentProvider>
     <Favicon url="assets/favicons/favicon.ico" />
     <Navbar />
     <AnimatedSwitch

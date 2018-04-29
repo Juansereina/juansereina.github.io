2baab70f953118611cec426827e6c25fb3c1b004
Juansereina <juan-2296@hotmail.es>
2018-04-28 21:58:22 -0500

    fixing hash links

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -7,24 +7,23 @@ const defaultStyleIcon = `${styles.icon} hvr-forward`;
 const active =  `${styles.iconActive} hvr-forward`;
 
 const NavBar = ({ props }) =>{
-  let currentLocation = window.location.pathname 
-
+  let currentLocation = window.location.hash 
   return  (
     <nav className={styles.root}>
       <div className={styles.logo}>
         <NavLink to="/" >
-          <FontAwesome name="home" className={ currentLocation == '/' ? active : defaultStyleIcon} />
+          <FontAwesome name="home" className={ currentLocation == '#/' ? active : defaultStyleIcon} />
         </NavLink>
       </div>
       <div className={styles.link}>
         <NavLink to="/about" >
-          <FontAwesome name="user-secret" className={ currentLocation == '/about' ? active : defaultStyleIcon} />
+          <FontAwesome name="user-secret" className={ currentLocation == '#/about' ? active : defaultStyleIcon} />
         </NavLink>
         <NavLink to="/work" >
-          <FontAwesome name="code" className={ currentLocation == '/work' ? active : defaultStyleIcon} />
+          <FontAwesome name="code" className={ currentLocation == '#/work' ? active : defaultStyleIcon} />
         </NavLink>
         <NavLink to="/contact" >
-          <FontAwesome name="paper-plane" className={ currentLocation == '/contact' ? active : defaultStyleIcon} />
+          <FontAwesome name="paper-plane" className={ currentLocation == '#/contact' ? active : defaultStyleIcon} />
         </NavLink>
       </div>
       <div className={styles.social}>

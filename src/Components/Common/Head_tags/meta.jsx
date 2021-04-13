3ba33af4ef178973004d6631990d734d4c23c6e9
Juansereina <juansereina@outlook.com>
2021-04-13 18:15:45 -0500

    Meta

--- src/Components/Common/Head_tags/meta.jsx
+++ src/Components/Common/Head_tags/meta.jsx
@@ -6,14 +6,13 @@ const Meta = () => (
   <Helmet>
     <meta charSet="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta property="og:type" content="website" />
-    <meta property="og:title" content="Juan Sebastian Reina" />
-    <meta property="og:type" content="Interactive.WebDeveloper" />
-    <meta property="og:url" content="https://juansebastianreina.herokuapp.com" />
+    <meta property="og:title" content="Juan Sebastián Reina" />
+    <meta property="og:type" content="Portfolio" />
+    <meta property="og:url" content="https://juansereina.com" />
     <meta property="og:image" content="" />
     <meta property="og:image:width" content="500" />
     <meta property="og:image:height" content="500" />
-    <meta property="og:image:alt" content="Juan Sebastian Interactive Media Designer" />
+    <meta property="og:image:alt" content="Juan Sebastian - FrontEnd developer" />
     <meta
     property="og:description"
     content={description}

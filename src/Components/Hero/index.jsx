330077afb6042b1f35b35cd18e698baf4aeb84a0
Juansereina <juansereina@outlook.com>
2021-04-14 20:35:08 -0500

    Typo and disable download button

--- src/Components/Hero/index.jsx
+++ src/Components/Hero/index.jsx
@@ -8,7 +8,8 @@ export default ({ id }) => (
     <div className={styles.container}>
       <h1 className={styles.title}>Juan Sebastián Reina</h1>
       <h2 className={styles.subtitle}>FrontEnd Developer</h2>
-      <button className={styles.cta}><FormattedMessage id="download" /></button>
+      {/* In development */}
+      {/* <button className={styles.cta}><FormattedMessage id="download" /></button> */}
       <img className={styles.image} src={image} alt="Juan Sebastian's photo" />
     </div>
   </header>

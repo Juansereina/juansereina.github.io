a255c415699e77cd1fb23a450cdf9302254fdaac
Juansereina <jsreina@arrivelogistics.com>
2022-01-23 00:18:58 -0500

    Work section - render improvements

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { IntlProvider, FormattedMessage } from 'react-intl';
-import Home from /* webpackPreload: true */ "../Components/Home";
-import Meta from "../Components/Common/Head_tags";
-import locale_en from "../translations/en.json";
-import locale_es from "../translations/es.json";
+import React from 'react';
+import { IntlProvider } from 'react-intl';
+import Home from /* webpackPreload: true */ '../Components/Home';
+import Meta from '../Components/Common/Head_tags';
+import locale_en from '../translations/en.json';
+import locale_es from '../translations/es.json';
 import './styles.scss';
 
 const App = () => {
@@ -12,7 +12,7 @@ const App = () => {
   const locale = isSpanish ? language : 'en';
   const messages = isSpanish ? locale_es : locale_en;
 
-  return  (
+  return (
     <IntlProvider locale={locale} messages={messages}>
       <Meta />
       <Home />
--- src/Components/Work/index.jsx
+++ src/Components/Work/index.jsx
@@ -1,100 +1,63 @@
-import React from "react";
-import { FormattedMessage } from "react-intl";
-import styles from "./styles.scss";
-import imageOne from "../../Assets/image-1.webp";
-import imageTwo from "../../Assets/image-2.webp";
-import imageThree from "../../Assets/image-3.webp";
+import React from 'react';
+import { FormattedMessage, useIntl } from 'react-intl';
+import styles from './styles.scss';
+import imageOne from '../../Assets/image-1.webp';
+import imageTwo from '../../Assets/image-2.webp';
+import imageThree from '../../Assets/image-3.webp';
 
-export default ({ id }) => {
-  const formatValues = { strong: chunk => <strong>{chunk}</strong> }
+const formatValues = { strong: (chunk) => <strong>{chunk}</strong> };
+const getExperience = (value, target) => {
+  return Object.keys(target).filter((key) => key.includes(value));
+};
+
+const Work = ({ experience, isCurrent, image }) => {
+  const intl = useIntl();
+  const experiences = getExperience(experience, intl.messages);
+  const last = experiences.length - 1;
+  const lastText = experiences[last];
+  const [company, summary, ...list] = experiences;
 
   return (
-    <div id={id} className={styles.root}>
+    <section className={styles.section}>
       <div className={styles.imageContainer}>
-        <img src={imageOne} className={styles.image} alt="A cup of coffee and a keyboard" />
+        <img
+          src={image}
+          className={styles.image}
+          alt="A cup of coffee and a keyboard"
+        />
       </div>
-      <section className={styles.content}>
+      <div className={styles.content}>
         <h3 className={styles.title}>
-          <FormattedMessage id="experience.huge" values={formatValues}/> - <span>Current job</span>
+          <FormattedMessage id={company} values={formatValues} />
+          {isCurrent && <span className={styles.current}> (current job)</span>}
         </h3>
         <p className={styles.text}>
-          <FormattedMessage id="experience.huge.1" values={formatValues}/>
+          <FormattedMessage id={summary} values={formatValues} />
         </p>
         <ul className={styles.list}>
-          <li className={styles.item}>
-            <FormattedMessage id="experience.huge.2" values={formatValues}/>
-          </li>
-          <li className={styles.item}>
-            <FormattedMessage id="experience.huge.3" values={formatValues}/>
-          </li>
-          <li className={styles.item}>
-            <FormattedMessage id="experience.huge.4" values={formatValues}/>
-          </li>
-          <li className={styles.item}>
-            <FormattedMessage id="experience.huge.5" values={formatValues}/>
-          </li>
+          {list.map(
+            (item, index) =>
+              index !== last && (
+                <li key={`item-${index}`} className={styles.item}>
+                  <FormattedMessage id={item} values={formatValues} />
+                </li>
+              )
+          )}
         </ul>
         <p className={styles.text}>
-          <FormattedMessage id="experience.huge.6" values={formatValues}/>
+          <FormattedMessage id={lastText} values={formatValues} />
         </p>
-      </section>
-      <section className={styles.content}>
-        <h3 className={styles.title}>
-          <FormattedMessage id="experience.psl" values={formatValues}/>
-        </h3>
-        <p className={styles.text}>
-          <FormattedMessage id="experience.psl.1" values={formatValues}/>
-        </p>
-        <ul className={styles.list}>
-          <li className={styles.item}>
-            <FormattedMessage id="experience.psl.2" values={formatValues}/>
-          </li>
-          <li className={styles.item}>
-            <FormattedMessage id="experience.psl.3" values={formatValues}/>
-          </li>
-          <li className={styles.item}>
-            <FormattedMessage id="experience.psl.4" values={formatValues}/>
-          </li>
-          <li className={styles.item}>
-            <FormattedMessage id="experience.psl.5" values={formatValues}/>
-          </li>
-          <li className={styles.item}>
-            <FormattedMessage id="experience.psl.6" values={formatValues}/>
-          </li>
-        </ul>
-        <p className={styles.text}>
-          <FormattedMessage id="experience.psl.7" values={formatValues}/>
-        </p>
-      </section>
-      <div className={styles.imageContainer}>
-        <img src={imageTwo} className={styles.image} values={formatValues} alt="Screenshot of Visual Studio Code"/>
-      </div>{" "}
-      <div className={styles.imageContainer}>
-        <img src={imageThree} className={styles.image} values={formatValues} alt="A Mac laptop computer" />
       </div>
-      <section className={styles.content}>
-        <h3 className={styles.title}>
-          <FormattedMessage id="experience.freelance" values={formatValues}/>
-        </h3>
-        <p className={styles.text}>
-          <FormattedMessage id="experience.freelance.1" values={formatValues}/>
+    </section>
+  );
+};
 
-        </p>
-        <ul className={styles.list}>
-          <li className={styles.item}>
-            <FormattedMessage id="experience.freelance.2" values={formatValues}/>
-          </li>
-          <li className={styles.item}>
-            <FormattedMessage id="experience.freelance.3" values={formatValues}/>
-          </li>
-          <li className={styles.item}>
-            <FormattedMessage id="experience.freelance.4" values={formatValues}/>
-          </li>
-        </ul>
-        <p className={styles.text}>
-          <FormattedMessage id="experience.freelance.5" values={formatValues}/>
-        </p>
-      </section>
+export default ({ id }) => {
+  return (
+    <div id={id} className={styles.root}>
+      <Work experience="huge" image={imageOne} isCurrent />
+      <Work experience="psl" image={imageTwo} />
+      <Work experience="freelance" image={imageThree} />
     </div>
   );
-}
+};

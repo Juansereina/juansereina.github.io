89a7df746fa626315145ae0efdc45fe7e76c28ed
Juansereina <juansereina@outlook.com>
2023-03-14 21:47:26 -0500

    revert

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -2,7 +2,7 @@ import { format } from 'date-fns';
 import { es } from 'date-fns/locale/index';
 
 export const formatDate = (date: Date) => {
-  const dateFormat = 'dd MMM, yyy';
+  const dateFormat = 'dd MMM, yyyy';
 
   return format(date, dateFormat, { locale: es });
 };

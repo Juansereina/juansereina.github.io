7863a7aeb07d470605633193ff2a67694e15b651
Juansereina <juansereina@outlook.com>
2023-03-14 22:37:27 -0500

    minor

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://juansereina.github.io',
+  site: 'https://juansereina.com',
   integrations: [
     tailwind(),
     partyTown({

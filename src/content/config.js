eae969220fa65a1ca13193e4261abd7bf1b957c3
Juansereina <juansereina@outlook.com>
2023-03-15 20:00:01 -0500

    author and keywords

--- src/content/config.js
+++ src/content/config.js
@@ -5,6 +5,7 @@ const blogCollection = defineCollection({
   schema: z.object({
     title: z.string(),
     description: z.string(),
+    keywords: z.string(),
     draft: z.boolean().optional(),
     image: z.object({
       src: z.string(),

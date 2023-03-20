57ef2f463df1e45fa94ccdce0a972d882366da48
Juansereina <juansereina@outlook.com>
2023-03-19 21:24:55 -0500

    darkmode toggle

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
+  darkMode: 'class',
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
       fontFamily: {
-        'sans': ['Roboto', 'sans-serif'],
-      }
+        sans: ['Roboto', 'sans-serif'],
+      },
     },
-	},
-	plugins: [],
-}
+  },
+  plugins: [],
+};

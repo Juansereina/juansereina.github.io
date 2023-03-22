cb2f17910514bc2c936d29450382ab632bd95454
Juansereina <juansereina@outlook.com>
2023-03-21 19:09:52 -0500

    Fix nav links

--- src/utils/route.ts
+++ src/utils/route.ts
@@ -5,3 +5,9 @@ export const stripLangFromSlug = (slug: CollectionEntry<'blog'>['slug']) =>
 
 export const getLangFromSlug = (slug: CollectionEntry<'blog'>['slug']) =>
   slug.split('/')[0];
+
+export function getLanguageFromURL(pathname: string) {
+  const langCodeMatch = pathname.match(/\/([a-z]{2}-?[a-z]{0,2})\//);
+
+  return langCodeMatch ? langCodeMatch[1] : 'es';
+}

4142aaf499e0bac1eb636209d66038eeba0963ed
Juansereina <juansereina@outlook.com>
2023-03-23 16:15:56 -0500

    remove modules

--- public/js/route.js
+++
@@ -1,6 +0,0 @@
-export const stripLangFromSlug = (slug) => {
-  const startWithSlash = slug.startsWith('/');
-  const sliceAt = startWithSlash ? 2 : 1;
-
-  return slug.split('/').slice(sliceAt).join('/');
-};
--- public/js/storage.js
+++
@@ -1,2 +0,0 @@
-export const saveLang = (lang) => localStorage.setItem('lang', lang);
-export const getCurrentLang = () => localStorage.getItem('lang');

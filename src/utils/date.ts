b41030eaf5120a376cea29ddf40e6c528e85bdd6
Juansereina <juansereina@outlook.com>
2023-03-23 20:42:23 -0500

    fix build

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,7 +1,8 @@
 import { Lang } from '@/types/lang';
 import { format } from 'date-fns';
-import es from 'date-fns/locale/es';
-import en from 'date-fns/locale/en-US';
+// @ts-ignore
+import { es } from 'date-fns/locale/index';
+import en from 'date-fns/locale/en-US/index';
 
 export const formatDate = (date: Date, lang: Lang) => {
   const dateFormat = 'dd MMM, yyyy';

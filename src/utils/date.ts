459f9f4ca0d5ed9bfea814ff4da58931456922ec
Juansereina <juansereina@outlook.com>
2023-03-23 20:31:31 -0500

    Time - i18n

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,8 +1,11 @@
+import { Lang } from '@/types/lang';
 import { format } from 'date-fns';
-import { es } from 'date-fns/locale/index';
+import es from 'date-fns/locale/es';
+import en from 'date-fns/locale/en-US';
 
-export const formatDate = (date: Date) => {
+export const formatDate = (date: Date, lang: Lang) => {
   const dateFormat = 'dd MMM, yyyy';
+  const locale = lang === Lang.es ? es : en;
 
-  return format(date, dateFormat, { locale: es });
+  return format(date, dateFormat, { locale });
 };

42d8d3896f6296490cffe09ea2ca8a202711d36f
Juansereina <juansereina@outlook.com>
2023-03-23 19:05:16 -0500

    sitemap

--- src/utils/route.ts
+++ src/utils/route.ts
@@ -6,11 +6,15 @@ export const stripLangFromSlug = (slug: CollectionEntry<'blog'>['slug']) =>
 export const getLangFromSlug = (slug: CollectionEntry<'blog'>['slug']) =>
   slug.split('/')[0];
 
-type Lang = 'en' | 'es';
+enum Lang {
+  en = 'en',
+  es = 'es',
+}
 
 export function getLanguageFromURL(pathname: string) {
   const langCodeMatch = pathname.match(/\/?([a-z]{2}-?[a-z]{0,2})\/?/);
-  const result = langCodeMatch ? langCodeMatch[1] : 'en';
+  const result = langCodeMatch ? langCodeMatch[1] : Lang.en;
+  const exist = result.includes(Lang.en || Lang.es);
 
-  return result as Lang;
+  return exist ? result : Lang.en;
 }
